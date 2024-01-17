b6a5f252e65b19cbc50ec15ef9ed53ec1473d55d
Nicholas Page <npage@southhills.edu>
2024-01-17 17:47:07 -0500

    Initial commit

---
+++ IT231-BlazorEF/IT231-BlazorEF/Program.cs
@@ -0,0 +1,41 @@
+using IT231_BlazorEF.Client.Pages;
+using IT231_BlazorEF.Components;
+
+namespace IT231_BlazorEF
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveWebAssemblyComponents();
+
+            WebApplication app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveWebAssemblyRenderMode()
+                .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+
+            app.Run();
+        }
+    }
+}
